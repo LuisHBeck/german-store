50597974c99fba31a282f2d37c3c9069e7e2f2dd
LuisHBeck <beck.henrique.luis@gmail.com>
2024-01-25 22:36:23 -0300

    feat: create and get service for user

--- back-end/user/src/main/java/com/store/user/controller/UserController.java
+++ back-end/user/src/main/java/com/store/user/controller/UserController.java
@@ -1,15 +1,32 @@
 package com.store.user.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.store.user.dto.UserRegistrationData;
+import com.store.user.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public ResponseEntity createUser(@RequestBody @Valid UserRegistrationData data, UriComponentsBuilder uriBuilder) {
+        var user = userService.createUser(data);
+        var uri = uriBuilder.path("/users/{id}").buildAndExpand(user.id()).toUri();
+        return ResponseEntity.created(uri).body(user);
+    }
+
     @GetMapping
-    public String userController() {
-        return "user controller";
+    public ResponseEntity list(@PageableDefault(size = 10, sort = {"id"}) Pageable pageable) {
+        var page = userService.listAllUsers(pageable);
+        return ResponseEntity.ok(page);
     }
 }
