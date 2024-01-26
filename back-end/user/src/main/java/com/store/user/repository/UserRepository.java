e6e5baed018211d9b8197c69cfa7842eb8262fd9
LuisHBeck <beck.henrique.luis@gmail.com>
2024-01-25 22:40:00 -0300

    feat: spring security auth with token jwt

--- back-end/user/src/main/java/com/store/user/repository/UserRepository.java
+++ back-end/user/src/main/java/com/store/user/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.store.user.repository;
 
 import com.store.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    UserDetails findByUsername(String username);
 }
--- back-end/user/src/main/java/com/store/user/service/UserService.java
+++ back-end/user/src/main/java/com/store/user/service/UserService.java
@@ -1,12 +1,39 @@
 package com.store.user.service;
 
+import com.store.user.dto.UserDetailingData;
+import com.store.user.dto.UserRegistrationData;
+import com.store.user.model.User;
 import com.store.user.repository.UserRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
     @Autowired
-    private UserRepository repository;
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Transactional
+    public UserDetailingData createUser(UserRegistrationData data) {
+        var encryptedPassword = encryptPassword(data.password());
+        var user = new User(data.username(), encryptedPassword);
+
+        userRepository.save(user);
+        return new UserDetailingData(user);
+    }
+
+    public Page<UserDetailingData> listAllUsers(Pageable pageable) {
+        return userRepository.findAll(pageable).map(UserDetailingData::new);
+    }
+
+    private String encryptPassword(String password) {
+        return passwordEncoder.encode(password);
+    }
 }
